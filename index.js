0a8ab009e397f3ba83033212fe5c5fb156ef324b
Milan Tomasovic <tomasovic@gmail.com>
2025-04-01 19:53:22 +0200

    workflow

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Petra Vice......");
+  res.send("Jel radi ovo sranje vise????");
 });
 
 app.listen(port, () => {
