5fb1a589805fcb6961fea535c6976fc2b41e1163
Milan Tomasovic <tomasovic@gmail.com>
2025-06-14 20:55:26 +0200

    testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Pozdrav mojoj zeni Tamari!!!!");
+  res.send("e kad ovo bude pravi servis, al ce pucaaaaa....");
 });
 
 app.listen(port, () => {
