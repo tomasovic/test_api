0bd95010987f5ad25f366cd8d9bc19eee6188ea4
Milan Tomasovic <tomasovic@gmail.com>
2025-06-14 21:12:17 +0200

    vibe

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("e kad ovo bude pravi servis, al ce pucaaaaa....");
+  res.send("Vibinggggggg.....");
 });
 
 app.listen(port, () => {
